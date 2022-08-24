6367b0e46f9e3fe6e3e128e575466ada38967a33
Daniele Barbosa <daanybsilva@gmail.com>
2022-08-23 21:53:44 -0300

    Aplicando Materialize css na aplicação

--- src/main/java/com/eventoapp/controllers/IndexController.java
+++ src/main/java/com/eventoapp/controllers/IndexController.java
@@ -1,13 +1,14 @@
 package com.eventoapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
     @RequestMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
 }
